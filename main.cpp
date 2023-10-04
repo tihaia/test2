16a6c126569b87f28458a21115fc5171394d4bea
tihaia <145658291+tihaia@users.noreply.github.com>
2023-10-04 23:46:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,17 @@ int main(){
 	i = 69;
 	int number = 72;
 	i = i + number;
-	int arr[2] = {0};
+	int arr[3] = {};
 	int* iPointer = &i;
-	
-	
-
-
+	number = *iPointer;
+	*iPointer = 15;
+	iPointer = &arr[0];
+	iPointer += 2;
+	arr[0] = 5;
+	*iPointer = 6;
+	std::cout << "i " << i << std::endl;
+    	std::cout << "number " << number << std::endl;
+    	std::cout << "arr[0] " << arr[0] << std::endl;
+    	std::cout << "arr[2] " << arr[2] << std::endl;
+ return 0;
 }
